7af7aa602a138589f40bbffa5a9a27a32642db39
Bakudi <107456253+Bakudi@users.noreply.github.com>
2023-10-30 19:09:49 -0500

    implementacion puntos basicos
    creada la clase plato

---
+++ Platos.java
@@ -0,0 +1,48 @@
+public class Platos {
+    String nombre;
+    String descripcion;
+    Tipoplato tipo;
+    int costo;
+    int tiempoprep;
+
+    public Platos(String nombre, String descripcion,Tipoplato tipo,int costo,int tiempoprep){
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.tipo = tipo;
+        this.tipo = tipo;
+        this.costo = costo;
+        this.tiempoprep = tiempoprep;
+
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public Tipoplato getTipo() {
+        return tipo;
+    }
+    public void setTipo(Tipoplato tipo) {
+        this.tipo = tipo;
+    }
+    public int getCosto() {
+        return costo;
+    }
+    public void setCosto(int costo) {
+        this.costo = costo;
+    }
+    public int getTiempoprep() {
+        return tiempoprep;
+    }
+    public void setTiempoprep(int tiempoprep) {
+        this.tiempoprep = tiempoprep;
+    }
+    
+}
---
+++ Tipoplato.java
@@ -0,0 +1,3 @@
+public enum Tipoplato {
+    entrada, bebida, platofuerte
+}
