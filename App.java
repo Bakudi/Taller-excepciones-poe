7e45065757998cdabf96c17c4f4c5976db1d4a10
harrison314790 <117177675+harrison314790@users.noreply.github.com>
2023-11-02 18:19:13 -0500

    Descripción
    implementación de las descripción de los platos

--- App.java
+++ App.java
@@ -1,10 +1,10 @@
 
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.util.ArrayList;
-
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -113,13 +113,32 @@ public class App extends JFrame implements ActionListener, ItemListener{
         platosEntradasBox.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                platoInfoTextArea.setText("");
+                String nombrePlato = platosEntradasBox.getSelectedItem().toString();
+                for (Platos plato : menu) {
+                    if (plato.getNombre().equals(nombrePlato)) {
+                        platoInfoTextArea.setText("Nombre: " + plato.getNombre() + "\nDescripción: " + plato.getDescripcion()
+                                + "\nTipo: " + plato.getTipo() + "\nCosto: $" + plato.getCosto()
+                                + "\nTiempo de preparación: " + plato.getTiempoprep() + " minutos");
+                        break;
+                    }
+                }
             }
         });
 
         platosBebidasBox.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                platoInfoTextArea.setText("");
+                String nombrePlato = platosBebidasBox.getSelectedItem().toString();
+                for (Platos plato : menu) {
+                    if (plato.getNombre().equals(nombrePlato)) {
+                        platoInfoTextArea.setText("Nombre: " + plato.getNombre() + "\nDescripción: " + plato.getDescripcion()
+                                + "\nTipo: " + plato.getTipo() + "\nCosto: $" + plato.getCosto()
+                                + "\nTiempo de preparación: " + plato.getTiempoprep() + " minutos");
+                        break;
+                    }
+                }
 
             }
         });
@@ -127,6 +146,16 @@ public class App extends JFrame implements ActionListener, ItemListener{
         platosFuertesBox.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                platoInfoTextArea.setText("");
+                String nombrePlato = platosFuertesBox.getSelectedItem().toString();
+                for (Platos plato : menu) {
+                    if (plato.getNombre().equals(nombrePlato)) {
+                        platoInfoTextArea.setText("Nombre: " + plato.getNombre() + "\nDescripción: " + plato.getDescripcion()
+                                + "\nTipo: " + plato.getTipo() + "\nCosto: $" + plato.getCosto()
+                                + "\nTiempo de preparación: " + plato.getTiempoprep() + " minutos");
+                        break;
+                    }
+                }
 
             }
         });
@@ -138,49 +167,6 @@ public class App extends JFrame implements ActionListener, ItemListener{
 
     public static void main(String[] args) throws Exception {
         App app = new App();
-
-import java.util.ArrayList;
-import java.util.Scanner;
-
-public class App extends JFrame implements ActionListener, ItemListener{
-
-    Container contenedor;
-    FlowLayout layout;
-    JLabel titulo;
-    JTextField campo;
-    JTextArea area1;
-    JScrollPane scroll;
-    JButton boton1, boton2;
-    JCheckBox caja1, caja2, caja3;
-
-    /*public App(){
-        contenedor = getContentPane();
-        layout = new FlowLayout();
-        contenedor.setLayout(layout);
-
-        titulo = new JLabel("Gestión de restaurante");
-        titulo.setFont(new Font("Monospaced", Font.CENTER_BASELINE, 25));        
-        contenedor.add(titulo);
-
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(500,600);
-        setVisible(true);
-    }*/
-
-    public static void main(String[] args) throws Exception {
-        /*App app = new App();*/
-        ArrayList <Platos> carrito = new ArrayList<Platos>();
-
-
-        /*Platos entrada1 = new Platos("Aros de cebolla", "Aros hechos con cebolla .__.", Tipoplato.entrada, 15000, 10);
-        Platos entrada2 = new Platos("Tostadas", "Tostadas de platano con ají", Tipoplato.entrada, 20000, 15);
-        Platos bebida1 = new Platos("Jugo de mango", "bebida de mango ._.", Tipoplato.entrada, 20000, 10);
-        Platos bebida2 = new Platos("Limonada cerezada", "Limonada con sabor a cereza", Tipoplato.entrada, 25000, 10);
-        Platos fuerte1 = new Platos("Parrillada", "Picada de varias carnes (pollo, res y cerdo) acompañado de papa y ensalada", Tipoplato.entrada, 60000, 25);
-        Platos fuerte2 = new Platos("Bandeja paisa", "plato de arroz, frijoles, chorizo, huevo, aguacate y arepa", Tipoplato.entrada, 90000, 35);*/
-
-        
-
     }
 
     @Override
